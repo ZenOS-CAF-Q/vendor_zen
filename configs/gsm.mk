b8b35e211cc7644063352a982d07cd3bad5ecd53
Anay Wadhera <anay1018@gmail.com>
2018-12-16 23:02:07 -0800

    vendor/zen: update APNs from crosshatch december factory image
    Thanks Anay
    
    Change-Id: I6b6220655650ae4d54266aacb228099ac683d9ad

--- configs/gsm.mk
+++ configs/gsm.mk
@@ -1,6 +1,6 @@
 # GSM APN list
 PRODUCT_COPY_FILES += \
-    vendor/zen/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/zen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 PRODUCT_COPY_FILES += \
     vendor/zen/prebuilt/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
