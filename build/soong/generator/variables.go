1a2112253627c8aaa47b5335a323d80b84347d59
Rashed97 <rashed@linux.com>
2019-01-27 14:36:34 -0500

    vendor/zen: Dynamically generate kernel headers using zen generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

---
+++ build/soong/generator/variables.go
@@ -0,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func zenExpandVariables(ctx android.ModuleContext, in string) string {
+	zenVars := ctx.Config().VendorConfig("zenVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if zenVars.IsSet(name) {
+			return zenVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}
--- configs/BoardConfigZEN.mk
+++ configs/BoardConfigZEN.mk
@@ -3,3 +3,5 @@ include vendor/zen/configs/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/zen/configs/BoardConfigQcom.mk
 endif
+
+include vendor/zen/configs/BoardConfigSoong.mk
