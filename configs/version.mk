bce628bce6f389e2755b02ae08acc3e4145c8082
wirmpolter <wirmpolter@gmail.com>
2019-03-23 23:54:54 -0400

    vendor/zen: Time for v2.0!!
    * Let's refacor ZenOS to reflect it's current status
    * Slightly update structure

--- configs/version.mk
+++ configs/version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ZEN_MOD_VERSION = 9.0
+ZEN_MOD_VERSION = v2.0
 
 ifndef ZEN_BUILD_TYPE
     ZEN_BUILD_TYPE := UNOFFICIAL
@@ -21,7 +21,7 @@ endif
 CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
 ifeq ($(ZEN_OFFICIAL),true)
-     LIST = $(shell curl -s https://raw.githubusercontent.com/Zen-OS/vendor_zen/pie/zen.devices)
+     LIST = vendor/zen/zen.devices
      FOUND_DEVICE =  $(filter $(CURRENT_DEVICE), $(LIST))
       ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
         IS_OFFICIAL=true
@@ -35,14 +35,11 @@ endif
 ZEN_VERSION := ZenOS-$(ZEN_MOD_VERSION)-$(CURRENT_DEVICE)-$(ZEN_BUILD_TYPE)-$(shell date -u +%Y%m%d)
 ROM_FINGERPRINT := ZenOS/$(PLATFORM_VERSION)/$(ZEN_BUILD_TYPE)/$(DATE)$(TIME)
 
-
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.zen.version=$(ZEN_VERSION) \
   ro.zen.releasetype=$(ZEN_BUILD_TYPE) \
   ro.mod.version=$(ZEN_MOD_VERSION) \
-  ro.zen.fingerprint=$(ROM_FINGERPRINT)
+  ro.zen.fingerprint=$(ROM_FINGERPRINT) \
+  ro.zen.display.version=$(ZEN_DISPLAY_VERSION)
 
 ZEN_DISPLAY_VERSION := ZenOS-$(ZEN_MOD_VERSION)-$(ZEN_BUILD_TYPE)
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-  ro.zen.display.version=$(ZEN_DISPLAY_VERSION)
