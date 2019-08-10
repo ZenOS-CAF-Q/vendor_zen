185ceaea08f6ba643972e42c7861a9e59cab658a
Rashed97 <rashed@linux.com>
2019-08-10 16:19:04 -0400

    vendor/zen: qcom: Add UM 4.14 platforms
    Change-Id: If246c0ec06de22097d369c17ce48aa1270bab19c

--- configs/BoardConfigQcom.mk
+++ configs/BoardConfigQcom.mk
@@ -8,7 +8,8 @@ BR_FAMILY := msm8909 msm8916
 UM_3_18_FAMILY := msm8937 msm8953 msm8996
 UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845
-UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY)
+UM_4_14_FAMILY := sm8150 sm6150
+UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)
 
 BOARD_USES_ADRENO := true
 
@@ -38,7 +39,7 @@ ifeq ($(call is-board-platform-in-list, $(UM_PLATFORMS)),true)
 endif
 
 # Enable DRM PP driver on UM platforms that support it
-ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)),true)
     TARGET_USES_DRM_PP := true
 endif
 
@@ -47,12 +48,12 @@ TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 21)
 
 # Mark GRALLOC_USAGE_PRIVATE_10BIT_TP as valid gralloc bits on UM platforms that support it
-ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
+ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY) $(UM_4_14_FAMILY)),true)
     TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS += | (1 << 27)
 endif
 
 # List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
+MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 sm6150 sm8150
 
 # Every qcom platform is considered a vidc target
 MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
@@ -78,7 +79,11 @@ else
 ifeq ($(call is-board-platform-in-list, $(UM_4_9_FAMILY)),true)
     QCOM_HARDWARE_VARIANT := sdm845
 else
+ifeq ($(call is-board-platform-in-list, $(UM_4_14_FAMILY)),true)
+    QCOM_HARDWARE_VARIANT := sm8150
+else
     QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+endif
 endif
 endif
 endif
