7cf5b5d0593409d319534717a9fe712e97fa753e
akhilnarang <akhilnarang.1999@gmail.com>
2019-09-19 00:50:00 -0400

    vendor/zen: remove vendorsetup
    This isn't used in Android 10, COMMON_LUNCH_CHOICES is used
    Keeping this results in
    
    vendor/zen/vendorsetup.sh:2: add_lunch_combo is obsolete. Use COMMON_LUNCH_CHOICES in your AndroidProducts.mk instead.
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- vendorsetup.sh
+++
@@ -1,4 +0,0 @@
-for combo in $(curl -s  https://raw.githubusercontent.com/ZenOS-CAF-Q/vendor_zen/ten-caf/zen.devices | sed -e 's/#.*$//' | awk '{printf "zen_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
